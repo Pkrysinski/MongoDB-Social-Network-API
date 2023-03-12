f43e0184fc62de05c3d6574f4eea0f88258953f7
Peter Krysinski <pakrysin@gmail.com>
2023-03-11 20:15:36 -0700

    Updating how default reactionID works

--- models/Reaction.js
+++ models/Reaction.js
@@ -7,7 +7,7 @@ const reactionSchema = new Schema(
     reactionID: [
         {
         type: Schema.Types.ObjectId,
-        default: new mongoose.Types.ObjectId,
+        default: () => new Types.ObjectId(),
         }
     ],
     reactionBody: {
