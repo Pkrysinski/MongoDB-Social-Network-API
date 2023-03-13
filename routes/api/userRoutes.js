a7655504f1ff4f22119ee9e92764a7143a7b3876
Peter Krysinski <pakrysin@gmail.com>
2023-03-12 18:56:59 -0600

    Adding logic to userController and userRoutes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/')
+    .get(getUsers)
+    .post(createUser)
+    ;
+
+// /api/users/:userId
+router.route('/:userId')
+    .get(getSingleUser)
+    .update(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
